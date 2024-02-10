ab00f854e5d60a20df9fdd1cebe89e1ecc07cf70
Gopal Sakhwala <Gopal.Sakhwala@einfochips.com>
2024-02-10 18:10:21 +0530

    refactored code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,15 @@ import { faHackerrank } from "@fortawesome/free-brands-svg-icons/faHackerrank";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 right-0 smp-4 bg-gray-1200 text-white lg:bottom-1/2">
-      <div className="flex flex-row sm:flex-col items-center gap-4">
-        <FooterLink icon={faGithub} link="https://github.com/your-github-username" />
-        <FooterLink icon={faLinkedin} link="https://linkedin.com/in/your-linkedin-username" />
-        <FooterLink icon={faTwitter} link="https://twitter.com/your-twitter-username" />
-        <FooterLink icon={faInstagram} link="https://instagram.com/your-instagram-username" />
-        <FooterLink icon={faHackerrank} link="https://hackerrank.com/your-hackerrank-username" />
+    <footer className="fixed bottom-0 right-1/4 z-20 lg:bottom-0 lg:right-5 lg:top-1/2 md:bottom-0 md:right-10 md:top-1/2 sm:bottom-0 sm:right-5 sm:top-1/2">
+      <div className="p-4 rounded-lg">
+        <div className="flex flex-row gap-4 sm:flex-col items-center lg:gap-10 sm:gap-4 md:gap-8">
+          <FooterLink icon={faGithub} link="https://github.com/gopal031119" />
+          <FooterLink icon={faLinkedin} link="https://www.linkedin.com/in/gopalsakhwala/" />
+          <FooterLink icon={faTwitter} link="https://twitter.com/GSakhwala" />
+          <FooterLink icon={faInstagram} link="https://www.instagram.com/gopal__sakhwala/" />
+          <FooterLink icon={faHackerrank} link="https://www.hackerrank.com/profile/gopalsakhwala18" />
+        </div>
       </div>
     </footer>
   );
