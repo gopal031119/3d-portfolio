f1ef484ae16ed9f97672b62638cfbb67a65420dd
Gopal Sakhwala <Gopal.Sakhwala@einfochips.com>
2024-02-16 15:34:22 +0530

    changed size of chunk in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 10000, // Set to a higher value based on your needs
+  },
 })
